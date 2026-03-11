500db76090926a973e6e93551f0a598ef0899504
Colheen0 <colheenn@gmail.com>
2026-03-11 10:42:28 +0100

    ajout du idle sur le model

--- src/Model.tsx
+++ src/Model.tsx
@@ -1,12 +1,25 @@
-import { useGLTF } from '@react-three/drei'
-import type { JSX } from 'react'
+import { useGLTF, useAnimations } from '@react-three/drei'
+import { useEffect, useRef, type JSX } from 'react'
+import * as THREE from 'three'
 
 export default function Model(props: JSX.IntrinsicElements['group']) {
-const { scene, animations } = useGLTF('/models/yu_narukami_p5r.glb')
-console.log("Animations dispo :", animations) 
-// Regarde dans la console de ton navigateur s'il y a des trucs dedans !  
+
+const groupRef = useRef<THREE.Group>(null!)
+
+const { scene, animations } = useGLTF('public/models/yu_narukami_p5r.glb')
+
+const { actions } = useAnimations(animations, groupRef)
+
+useEffect(() => {
+  if (actions ['Take 001']) {
+    actions['Take 001'].play()
+  }
+}, [actions])
+
   return (
-    <primitive object={scene} {...props} />
+    <group ref={groupRef} {...props}>
+        <primitive object={scene} />
+    </group>
   )
 }
 
